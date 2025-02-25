fa76cc317fe4c4c923f64d93b4116ede26255b29
Harsh Gangwal <160650549+Harshgangwal10@users.noreply.github.com>
2025-02-25 17:16:41 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-content">
-        <h2>Welcome to My Store Your Shopping Destination</h2>
+        <h2>Welcome to My Cart Your Shopping Destination</h2>
         <p>"Discover a wide range of products just for you Shop with ease and find exactly what you need !"</p>
        
       </div>
